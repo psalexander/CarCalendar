453c6eb0e72c3e1e00918de4ea18ff2c2304b572
Alexander <psaaa@yandex.ru>
2015-12-11 17:32:06 +0400

    sdf

--- app/src/main/java/com/asinenko/carcalendar/CarActivity.java
+++ app/src/main/java/com/asinenko/carcalendar/CarActivity.java
@@ -1,23 +1,42 @@
 package com.asinenko.carcalendar;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+//import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.asinenko.carcalendar.items.MeasurementItem;
 import com.asinenko.carcalendar.items.OdometerItem;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
@@ -28,8 +47,10 @@ public class CarActivity extends AppCompatActivity {
     private TextView carVin;
     private TextView carNumber;
 
+    private String carName;
+
     private Realm realm;
-    private RealmResults<MeasurementItem> allMeasuresListResult;
+    private RealmResults<OdometerItem> allOdometerListResult;
     private Activity activity;
     private ListView listview;
     private OdometerArrayAdapter adapter;
@@ -45,6 +66,7 @@ public class CarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = this;
         setContentView(R.layout.activity_car);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -58,6 +80,9 @@ public class CarActivity extends AppCompatActivity {
         String vin = intent.getStringExtra("vin");
         String number = intent.getStringExtra("number");
 
+        carName = name;
+
+
         carVin.setText(vin);
         carNumber.setText(number);
         setTitle(name);
@@ -66,26 +91,100 @@ public class CarActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showAddMeasureDialog();
             }
         });
-
         realm = Realm.getDefaultInstance();
         realm.addChangeListener(realmCarListener);
-        allMeasuresListResult = realm.where(MeasurementItem.class).findAll();
-
-        adapter = new OdometerArrayAdapter(this, allMeasuresListResult);
-        listview = (ListView) findViewById(R.id.measureListView);
+        allOdometerListResult = realm.where(OdometerItem.class).findAll();
+        adapter = new OdometerArrayAdapter(this, allOdometerListResult);
+        listview = (ListView) findViewById(R.id.carOdometerListView);
         listview.setAdapter(adapter);
         registerForContextMenu(listview);
-
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //editMeasure(position);
+            //editMeasure(position);
             }
         });
+    }
+
+    private EditText odometerValueEditView;
+    private TextView odometerDateEditView;
+    private Button odometerButton;
+
+    public void showAddMeasureDialog(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View view = inflater.inflate(R.layout.add_km_dialog, null);
+
+        odometerValueEditView = (EditText) view.findViewById(R.id.odometerValueEdit);
+        odometerDateEditView = (TextView) view.findViewById(R.id.odometerDateEdit);
+        odometerButton = (Button) view.findViewById(R.id.setDateButton);
+
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        final Calendar c = Calendar.getInstance();
+        String str = format.format(c.getTime());
+        odometerDateEditView.setText(str);
+
+        odometerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment(odometerDateEditView);
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        final AlertDialog dialog = builder.setTitle("Добавить показаниe").setView(view).
+                setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int id) {
+                        realm.beginTransaction();
+
+                        OdometerItem item = realm.createObject(OdometerItem.class); // Create a new object
+
+                        item.setValue(odometerValueEditView.getText().toString());
+//                        item.setDate(new Date() odometerDateEditView.getText().toString());
+                        item.setCarname(carName);
+                        realm.commitTransaction();
+                    }
+                }).setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int id) {
+
+            }
+        }).create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+            }
+        });
+
+        odometerValueEditView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                RealmResults<MeasurementItem> result = realm.where(MeasurementItem.class).equalTo("name", text).findAll();
+                if (result.size() > 0 || text.equalsIgnoreCase("")) {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                } else {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                }
+            }
+        });
+        dialog.show();
     }
 
     public class OdometerArrayAdapter extends BaseAdapter {
@@ -115,21 +214,45 @@ public class CarActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.list_measure_item, parent, false);
-            TextView nameView = (TextView) rowView.findViewById(R.id.measureNameTextView);
-            TextView typeView = (TextView) rowView.findViewById(R.id.measureTypeTextView);
-            TextView intervalView = (TextView) rowView.findViewById(R.id.measureIntervalTextView);
-//            MeasurementItem measure = valuesResult.get(position);
-//            nameView.setText(measure.getName());
-//
-//            if (measure.getTimeType() != null && measure.getTimeType().equalsIgnoreCase("Время")){
-//                typeView.setText(" дн");
-//            }else if (measure.getTimeType() != null && measure.getTimeType().equalsIgnoreCase("Дистанция")){
-//                typeView.setText(" км");
-//            }
-//            intervalView.setText(String.valueOf(measure.getStandartInterval()));
+            View rowView = inflater.inflate(R.layout.list_odometer_item, parent, false);
+            TextView dateView = (TextView) rowView.findViewById(R.id.kmDateTextView);
+            TextView valueView = (TextView) rowView.findViewById(R.id.kmValueTextView);
+            OdometerItem item = valuesResult.get(position);
+            dateView.setText(item.getDate().toString());
+            valueView.setText(item.getValue());
             return rowView;
         }
     }
 
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        private TextView editText;
+
+        @SuppressLint("ValidFragment")
+        public DatePickerFragment(TextView edit_text){
+            editText = edit_text;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            editText.setText("Idi na huj");
+
+
+
+            String str = (day < 10 ? "0" + day : day) + "." + (month < 10 ? "0" + month : month) + "." + year;
+            editText.setText(str);
+        }
+    }
 }
